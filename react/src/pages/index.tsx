e9e1c5628d5fd37d550c2c6e4f6888eb3a0d07cc
Damian Nowacki <bobimaster15@gmail.com>
2023-11-23 23:41:02 +0000

    [main][story] placeholders

--- react/src/pages/index.tsx
+++ react/src/pages/index.tsx
@@ -1,14 +1,21 @@
 import styles from '@/styles/index.module.css'
-import { useContext, useEffect, useRef, useState } from 'react'
+import { Dispatch, ReactNode, SetStateAction, useContext, useEffect, useRef, useState } from 'react'
 
 import Head from 'next/head';
 import Link from 'next/link';
 import { PlayerContext } from './_app';
 import JSONg from 'jsong-audio';
 
-export default function Home({setIntroDone}) {
+export default function Home({setIntroDone} : {setIntroDone : Dispatch<SetStateAction<boolean>>}) {
 
   const player = useContext(PlayerContext) as JSONg;
+
+  const StartButton = ({children} : {children : ReactNode})=><button className={styles.button}
+  onClick={()=>{
+    player.play();
+    setIntroDone(true);
+  }} 
+  >{children}</button>
 
   return (
     <>
@@ -20,18 +27,17 @@ export default function Home({setIntroDone}) {
           What is JSONg Audio?
         </h1>
         <Link href="content">
-        <button className={styles.button}
-        onClick={()=>{
-          player.play();
-          setIntroDone(true);
-        }} 
-        >Let&apos;s hear it!</button>
+          <StartButton >Let&apos;s hear it!</StartButton>
         </Link>
-        <button className={styles.button}
+        <span>- or -</span>
+        <Link href="story">
+          <StartButton >Hear use case</StartButton>
+        </Link>
+        {/* <button className={styles.button}
         onClick={()=>{
           player.stop(0);
         }} 
-        >Stop</button>
+        >Stop</button> */}
         {/* <button onClick={()=>{
           player.current?.stop()
         }}>Stop</button> */}
