b6d0761d110b1328f470fd36af83e8659fe5a2ba
purewack <bobimaster15@gmail.com>
2025-01-23 18:29:33 +0000

    try workflow for next.js

--- react/next.config.js
+++ react/next.config.js
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  assetPrefix: './',
-  reactStrictMode: true,
+  output: "export",
+  basePath: "/jsong-examples/react",
+  images: {
+    unoptimized: true,
+  },
   transpilePackages: ['jsong-audio']
 }
 
